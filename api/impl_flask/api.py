249195a90bb3e50321dd6abe297bc954dccfae13
mana <mana@azarm.ca>
2019-07-22 10:36:01 -0400

    added the after_request method to add response headers

--- api/impl_flask/api.py
+++ api/impl_flask/api.py
@@ -34,6 +34,12 @@ def __parse_bool(s):
         return False
     return s.lower() in ('true', '1')
 
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
 
 @app.route('/login', methods=['GET'])
 def authenticate():
